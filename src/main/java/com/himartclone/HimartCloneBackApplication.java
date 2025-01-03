e5a5ef3f71c3da4505aa1da04cd50050af242642
juhyung.park <kkk26kkk@naver.com>
2025-01-03 14:59:19 +0900

    init

---
+++ src/main/java/com/himartclone/HimartCloneBackApplication.java
@@ -0,0 +1,13 @@
+package com.himartclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimartCloneBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HimartCloneBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/himartclone/HimartCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.himartclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimartCloneBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
